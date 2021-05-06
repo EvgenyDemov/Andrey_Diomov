70263c10787bfbdbedbf7304cd73e109329697ee
Andrey Diomov <andrey-tgsv@mail.ru>
2021-05-06 13:41:06 +0300

    commit

--- My_App/main.js
+++ My_App/main.js
@@ -1,46 +1,33 @@
-function addNewRow() {
-    alert("Hello");
-}
-
 function addRow(tableID) {
-
     var table = document.getElementById(tableID);
 
-    var rowCount = table.rows.length;
-    var row = table.insertRow(rowCount);
+    var rowCount = table.rows.length; // row
+    var row = table.insertRow(rowCount); //insertRow
 
     var cell1 = row.insertCell(0);
-    var element1 = document.createElement("input");
-    element1.type = "checkbox";
-    element1.name = "chkbox[]";
-    cell1.appendChild(element1);
+    cell1.innerHTML = rowCount;
 
+    var nameVal = document.getElementById("name").value; //value
     var cell2 = row.insertCell(1);
-    cell2.innerHTML = rowCount + 1;
+    cell2.innerHTML = nameVal;
 
+    var dateVal = document.getElementById("date").value;
     var cell3 = row.insertCell(2);
-    var element2 = document.createElement("input");
-    element2.type = "text";
-    element2.name = "txtbox[]";
-    cell3.appendChild(element2);
+    cell3.innerHTML = dateVal;
 
+    var cell4 = row.insertCell(3);
+    var element4 = document.createElement("button");
+    element4.textContent = "Delete";
+    element4.addEventListener('click', function(e) { deleteRow(row.rowIndex) });
+    cell4.appendChild(element4);
+
+    var cell5 = row.insertCell(4);
+    var element5 = document.createElement("button");
+    element5.textContent = "Update";
+    cell5.appendChild(element5);
 }
 
-function deleteRow(tableID) {
-    try {
-        var table = document.getElementById(tableID);
-        var rowCount = table.rows.length;
-
-        for (var i = 0; i < rowCount; i++) {
-            var row = table.rows[i];
-            var chkbox = row.cells[0].childNodes[0];
-            if (null != chkbox && true == chkbox.checked) {
-                table.deleteRow(i);
-                rowCount--;
-                i--;
-            }
-        }
-    } catch (e) {
-        alert(e);
-    }
+function deleteRow(i) {
+    var table = document.getElementById('tableID');
+    table.deleteRow(i);
 }
